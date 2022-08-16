b3ec95f935b4754d15b7cc48623b20cb419be83a
machack1512 <2019pcecsshahrin163@poornima.org>
2022-08-16 14:51:41 +0530

    tba2

--- assignment-9/script.js
+++ assignment-9/script.js
@@ -13,3 +13,36 @@ function Fun()
    }
    document.getElementById("demo").innerHTML = "SUM : " + sum;
 }
+function Fun1()
+{
+    let a = parseInt(prompt("Enter the value: "));
+    let b = parseInt(prompt("Enter the value: "));
+    document.getElementById("demo").innerHTML = "RETURN 1 : " + (a+b);
+    document.getElementById("demo1").innerHTML = "RETURN 2 : " + (a-b);
+}
+function Fun2()
+{
+    console.log("hello this is the content of function!")
+    document.getElementById("demo").innerHTML = String(Fun2);
+}
+function Fun3()
+{
+    let str = prompt("Enter the String: ");
+    let part = str.split(" ");
+    if (part[0] == 'New!')
+    {
+        document.getElementById("demo1").innerHTML = str.slice(5);
+    }
+    else
+    {
+        let tab = "New! " + str;
+        document.getElementById("demo").innerHTML = tab;
+    }
+}
+function Fun4()
+{
+    let str = prompt("Enter the String: ");
+    let first = str.slice(0,3);
+    let last = str.slice(str.length-3);
+    document.getElementById("demo").innerHTML = first + last;
+}
