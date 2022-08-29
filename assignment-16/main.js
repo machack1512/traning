703ddbeef714536b92f30f13faf521eedb6b2f68
machack1512 <2019pcecsshahrin163@poornima.org>
2022-08-29 18:11:28 +0530

    final

--- assignment-16/main.js
+++ assignment-16/main.js
@@ -6,6 +6,10 @@ var coinImage, coinsGroup;
 var coinScore = 0;
 var coinSound;
 var jumpSound;
+var mushObstacleImage, turtleObstacleImage, obstaclesGroup;
+var gameState = "PLAY";
+var restartImg;
+var mario_collided;
 
 function preload() {
     bgImage = loadImage("img/bgnew.jpg");
@@ -28,6 +32,26 @@ function preload() {
     brickImage = loadImage("img/brick.png");
     coinSound = loadSound("sounds/coinSound.mp3");
     jumpSound = loadSound("sounds/sounds_jump.mp3");
+    mushObstacleImage = loadAnimation(
+        "img/mush1.png",
+        "img/mush2.png",
+        "img/mush3.png",
+        "img/mush4.png",
+        "img/mush5.png",
+        "img/mush6.png"
+      );
+      turtleObstacleImage = loadAnimation(
+        "img/tur1.png",
+        "img/tur2.png",
+        "img/tur3.png",
+        "img/tur4.png",
+        "img/tur5.png"
+      );
+      mario_collided = loadAnimation("img/dead.png");
+    
+      dieSound = loadSound("sounds/dieSound.mp3");
+    
+      restartImg = loadImage("img/restart.png");
   }
   function setup() {
     createCanvas(1000, 600);
@@ -40,8 +64,18 @@ function preload() {
     ground = createSprite(200, 580, 400, 10);
     bricksGroup = new Group();
     coinsGroup = new Group();
+    obstaclesGroup = new Group();
+
+    mario.addAnimation("collided", mario_collided);
+  
+    restart = createSprite(500, 300);
+    restart.addImage(restartImg);
+    restart.visible = false
   }
   function draw() {
+    drawSprites();
+
+    if (gameState == "PLAY") {
     bg.velocityX = -5;
     if (bg.x < 100) bg.x = bg.width / 4;
     if (keyDown("space")) mario.velocityY = -10;
@@ -73,11 +107,39 @@ function preload() {
       temp = null;
     }
   }
-    drawSprites();
+  generateObstacles();
+    if (obstaclesGroup.isTouching(mario)) {
+      dieSound.play();
+      gameState = "END";
+    }
+  } 
+  
+  else if (gameState === "END") {
+    bg.velocityX = 0;
+    mario.velocityY = 0;
+    mario.velocityX = 0;
+
+    obstaclesGroup.setVelocityXEach(0);
+    coinsGroup.setVelocityXEach(0);
+    bricksGroup.setVelocityXEach(0);
+
+    bricksGroup.setLifetimeEach(-1);
+    coinsGroup.setLifetimeEach(-1);
+    obstaclesGroup.setLifetimeEach(-1);
+
+    mario.changeAnimation("collided", mario_collided);
+    mario.y = 570;
+    mario.scale = 0.4;
+
+    restart.visible = true;
+    if (mousePressedOver(restart)) {
+      restartGame();
+    }
+  }
     textSize(20);
     fill("brown");
     text("Coins Collected: " + coinScore, 500, 50);
-  }
+}
   function generateBricks() {
     if (frameCount % 80 == 0) {
       console.log(frameCount);
@@ -102,3 +164,38 @@ function preload() {
       coinsGroup.add(coin);
     }
   }
+  
+function generateObstacles() {
+    if (frameCount % 100 === 0) {
+      var obstacle = createSprite(1200, 555, 10, 40);
+      obstacle.velocityX = -5;
+      obstacle.scale = 0.1;
+      var rand = Math.round(random(1, 2));
+      switch (rand) {
+        case 1:
+          obstacle.addAnimation("mush", mushObstacleImage);
+          break;
+        case 2:
+          obstacle.addAnimation("turtle", turtleObstacleImage);
+          break;
+        default:
+          break;
+      }
+      obstacle.lifetime = 300;
+      obstaclesGroup.add(obstacle);
+    }
+  }
+  function restartGame() {
+    gameState = "PLAY";
+  
+    obstaclesGroup.destroyEach();
+    bricksGroup.destroyEach();
+    coinsGroup.destroyEach();
+  
+    mario.changeAnimation("running", mario_running);
+    mario.scale = 0.2;
+  
+    coinScore = 0;
+  
+    restart.visible = false;
+}
